9bf40df769aea45481f4d60f2dd9d99abf28669d
Mun-GitHub <murugavel.natarajan@contentstack.com>
2025-11-19 12:44:39 +0530

    Removed local cache

--- lib/contentstack.ts
+++ lib/contentstack.ts
@@ -44,6 +44,8 @@ export const blogStack = contentstack.stack({
   // Setting the host for content delivery based on the region or environment variables
   host: process.env.NEXT_PUBLIC_CONTENTSTACK_CONTENT_DELIVERY || endpoints && endpoints.contentDelivery,
 
+  branch: process.env.NEXT_PUBLIC_CONTENTSTACK_BRANCH as string,
+
   live_preview: {
     // Enabling live preview if specified in environment variables
     enable: process.env.NEXT_PUBLIC_CONTENTSTACK_PREVIEW === 'true',
